25d08f73d77c5c072f9e3161891bac74a6241dc5
hlee260 <82513993+hlee260@users.noreply.github.com>
2021-04-15 16:15:45 -0400

    Update phrgbgenerator.py

--- phrgbgenerator.py
+++ phrgbgenerator.py
@@ -12,25 +12,23 @@ import glob
 import os
 import csv
 
-# Open a file to write the pixel data
+# We need to open a csv file to write the pH and rgb data
 with open('o.csv', 'w', newline='') as f_output:
     csv_output = csv.writer(f_output)
     csv_output.writerow(["ph", "R", "G", "B"])
 
-    # Path to file
+    # We need to create a path to the file with png files labeled with pH in the range of 0-14
     for filename in glob.glob("*.png"):
         im = Image.open(filename)
         img_name = os.path.basename(filename)
-
-        # Load the pixel info
-        pxl = im.load()
+        pxl = im.load() # generate RGB values
 
         # Get a tuple of the x and y dimensions of the image
         width, height = im.size
-
+        # Verify all image files labeled with pH in the range of 0-14 have run through the code.
         print("Getting RGB values from reference pH: " + filename)
-
-        # Read the details of each pixel and write them to the file
+        
+        # Read the details of each pixel and write them to the csv file
         for x in range(width):
             for y in range(height):
                 R = pxl[x, y][0]
