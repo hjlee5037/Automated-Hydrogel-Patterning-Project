a44acc6d16c2377b1cf5633f00f8474ec132bdf0
hlee260 <82513993+hlee260@users.noreply.github.com>
2021-04-22 17:01:19 -0400

    Update and rename phcalculator.py to RFR_model.py

--- RFR_model.py
+++ RFR_model.py
@@ -13,9 +13,8 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from PIL import Image
-import numpy as np
 import joblib
+
 
 def build_network(x_train, y_train, x_test, y_test):
     '''
@@ -23,13 +22,13 @@ def build_network(x_train, y_train, x_test, y_test):
     After building the model, the model is then trained, saved, and its performance is outputted.
 
     **Parameters**
-        x_train: *numpy*
+        x_train: 
             The set of training data, which should be 80% from dataset (39123 data points).
-        y_train: *numpy*
+        y_train:
             The set of labels for the corresponding training data, which is 80% from dataset.
-        x_test: *numpy.ndarray*
+        x_test:
             The set of testing data, which is 20% from dataset (9781 data points).
-        y_test: *numpy.ndarray*
+        y_test:
             The set of labels for the corresponding testing data, which is 20% from dataset.
     **Returns**
         model:
@@ -41,7 +40,7 @@ def build_network(x_train, y_train, x_test, y_test):
     y_predict = model.predict(x_test)
     # Save the model
     joblib.dump(model, "./pH_model.joblib")
-    print("Model saved.")
+    print("Model saved as pH_model.joblib.")
     # calculate MAE and MSE of model
     MAE = metrics.mean_absolute_error(y_test, y_predict)
     print('MAE: ' + str(MAE))
@@ -57,58 +56,6 @@ def build_network(x_train, y_train, x_test, y_test):
     fig.savefig("PH_performance.png")
     return model
 
-def pH_predict(x):
-    '''
-    This function loads the saved model to predict pH of an input x value.
-
-    **Parameters**
-        x_train: *numpy*
-            The set of training data.
-        y_train: *numpy*
-            The set of labels for the corresponding training data.
-        x: *numpy.ndarray*
-            An input value to predict pH.
-    **Returns**
-        pH_estimate:
-            Estimated pH value.
-    '''
-    # Load the saved model.
-    loaded_model = joblib.load("./pH_model.joblib")
-    # Use model to predict pH of input value.
-    pH_estimate = loaded_model.predict(np.array([x]).reshape(1, 1))
-    return pH_estimate
-
-def recognize_pH():
-    '''
-    This function retreives RGB from a single pixel of a certain image,
-    calculates the weighted grayscale of the image, then inputs the normalized value
-    into pH_predict function. The predicted pH value for the image is printed out.
-    
-    **Parameters**
-        x_train: *numpy*
-            The set of training data.
-        y_train: *numpy*
-            The set of labels for the corresponding training data.
-        x: *numpy.ndarray*
-            An input value to predict pH.
-    **Returns**
-        pH_estimate:
-            Estimated pH value.
-    '''
-    #input of file name of image with specific pH value and single color.
-    filename = '9.7.png'
-    img = Image.open(filename)
-    # Parallel to x values from data, we calculate weighted grayscale (c) of a single pixel in a image.
-    pxl = img.getpixel((1,1))
-    c = (((0.299 * int(pxl[0])) + (0.587 * int(pxl[1]))
-            + (0.114 * int(pxl[2]))))
-    # Normalize the c value between 0 and 1.
-    c_norm = int(c) / 255
-    #Use pH_predict to calculate pH using model and c_norm.
-    predicted_pH = pH_predict(c_norm)
-    # Print the file name and predicted pH value according to the model.
-    print(f'{filename}, {predicted_pH}')
-    return
 
 if __name__ == '__main__':
     # Suppress TensorFlow warnings
@@ -137,5 +84,3 @@ if __name__ == '__main__':
 
     # Build network
     build_network(x_train, y_train, x_test, y_test)
-    print("pH prediction: ")
-    recognize_pH()
