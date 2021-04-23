2d220b382b5acd02a55f498aa73c8590eaa4f820
hlee260 <82513993+hlee260@users.noreply.github.com>
2021-04-22 22:49:18 -0400

    Update pH_predict.py

--- pH_predict.py
+++ pH_predict.py
@@ -1,9 +1,15 @@
+'''
+This function is used to open up a pH scale image, and
+predict the pH of clicked regions using the model from
+RFR_model.py.
+'''
+
+import cv2
 import joblib
-from PIL import Image
 import numpy as np
 
 
-def pH_predict(x):
+def pH_predict(c_norm):
     '''
     This function loads the saved model to predict pH of an input x value.
     **Parameters**
@@ -16,40 +22,49 @@ def pH_predict(x):
     # Load the saved model.
     loaded_model = joblib.load("./pH_model.joblib")
     # Use model to predict pH of input value.
-    pH_estimate = loaded_model.predict(np.array([x]).reshape(1, 1))
+    pH_estimate = loaded_model.predict(np.array([c_norm]).reshape(1, 1))
     return pH_estimate
 
 
-def recognize_pH(filename):
+def mouse_pH(event, x, y, flags, param):
     '''
-    This function retreives RGB from a single pixel of a certain image,
-    calculates the weighted grayscale of the image, then inputs the normalized value
-    into pH_predict function. The predicted pH value for the image is printed out.
-
+    This function identifies x,y positions of click, reads RGB values,
+    converts to weighted grayscale, and predicts the pH using pH_predict function.
     **Parameters**
-        filename: *numpy.ndarray*
-            An input value to predict pH.
-    **Returns**
-        pH_estimate:
-            Estimated pH value.
+        event:
+            Used to define mouse clicking
+        x:
+            x position of the mouse click.
+        y:
+            y position of the mouse click.
+        flags:
+            Indication of how images should be read.
+        para:
+            Takes in additional variables.
+
     '''
-    # input of file name of image with specific pH value and single color.
-    img = Image.open(filename)
-    # Parallel to x values from data, we calculate weighted grayscale (c) of a single pixel in a image.
-    pxl = img.getpixel((1, 1))
-    c = (((0.299 * int(pxl[0])) + (0.587 * int(pxl[1]))
-          + (0.114 * int(pxl[2]))))
+    if event == cv2.EVENT_LBUTTONDOWN:  # mouse left button down condition
+        # retrieve RGB values and calculate weighted grayscale
+        b = img[y, x, 0]
+        g = img[y, x, 1]
+        r = img[y, x, 2]
+        c = (((0.299 * r)) + (0.587 * g)) + (0.114 * b)
     # Normalize the c value between 0 and 1.
-    c_norm = int(c) / 255
+        c_norm = int(c) / 255
     # Use pH_predict to calculate pH using model and c_norm.
-    predicted_pH = pH_predict(c_norm)
-    # Print the file name and predicted pH value according to the model.
-    print(f'{filename}, {predicted_pH}')
-    img.show()
-    return
+        predicted_pH = pH_predict(c_norm)
+        print("pH of clicked region: ", predicted_pH)
 
 
-filename = '9.7.png'
+# Read an image, opens a window and bind the mouse_pH function to the window
+img = cv2.imread("pH_scale.png")
+cv2.namedWindow('mouse_pH')
+cv2.setMouseCallback('mouse_pH', mouse_pH)
 
-print("pH prediction:")
-recognize_pH(filename)
+# Do until esc pressed
+while(1):
+    cv2.imshow('mouse_pH', img)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+# if esc pressed, finish.
+cv2.destroyAllWindows()
